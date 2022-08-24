b496467cfc8167df7e9e14442c2464d50da4e913
DiviyamPathak <patdiviyam@gmail.com>
2022-08-24 17:39:13 +0530

    all done except design and css

--- src/App.js
+++ src/App.js
@@ -1,39 +1,73 @@
-import Button from './components/Button';
+
 import './App.css';
 import Header from './components/Header';
 import Tasks from './components/Tasks';
-import { useState } from 'react';
+import { useState , useEffect} from 'react';
 import Add from './components/Add';
 
 function App() {
-  let [db , setDb]= useState([
-    {
-    "id": 1,
-    "text": "Doctors Appointment",
-    "day": "Feb 5th at 2:30pm",
-    "reminder": true
-        },
-    {
-    "id": 2,
-    "text": "Meeting at School",
-    "day": "Feb 6th at 1:30pm",
-    "reminder": true
-        }
-  ])
+  let [db , setDb]= useState([])
     
-  const togglerem = (id) => {
-    setDb(db.map((task)=> task.id === id ?{...task,reminder:!task.reminder}:task ))
+  useEffect( ()=>{
+    const gettasks = async ()=> {
+      const datare = await fetchtasks()
+      setDb(datare)
+    }
+
+   gettasks()
+  },[])
+
+  const fetchtasks = async ()=> {
+    const data = await fetch("http://localhost:5000/tasks")
+    const resdata = await data.json()
+    return resdata 
+  }
+
+  const togglerem = async (id) => {
+    const tasktobeupdated = await fetch(`http://localhost:5000/tasks/${id}`)
+    const update = await tasktobeupdated.json()
+    const oudated = {...update, reminder:!update.reminder}
+    const res = await fetch(`http://localhost:5000/tasks/${id}`,{
+      method:'PUT',
+      headers:{
+        'Content-type':'application/json',
+      },
+      body:JSON.stringify(oudated)
+
+    })
+
+    const data = await res.json()
+
+    
+    setDb(db.map((task)=>task.id=== id ? {...task,reminder:data.reminder }: task))
     
   }
   
-  const addtaskst = (taski) => {
-    const id = Math.floor(Math.random() * 10000) + 1
-    const newtask = {id, ...taski}
-    setDb([...db, newtask])
+  const addtaskst = async (task) => {
+    const res = await fetch('http://localhost:5000/tasks',{
+      method:"POST",
+      headers:{
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify(task),
+    })
+    const data = await res.json()
+    setDb([...db, data])
+    // const id = Math.floor(Math.random() * 10000) + 1
+    // const newtask = {id, ...taski}
+    // setDb([...db, newtask])
   }
   
-  const ondel = (id) => {
-    setDb(db.filter((task)=>task.id !== id))
+  const ondel = async (id) => {
+    const res = await fetch(`http://localhost:5000/tasks/${id}`,{
+      method:'DELETE',
+    })
+    res.status === 200
+      ? setDb(db.filter((task) => task.id !== id))
+      : alert('Error Deleting This Task')
+    //  setDb(db.filter(function (task) {
+    //     return task.id !== id;
+    //   }))
      
   }
   return (
